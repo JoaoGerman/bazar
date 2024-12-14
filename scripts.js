0371475001cfe4c3448541333f4d4fd8dd93f3cc
JoaoGerman <joaogermano2730@gmail.com>
2024-12-14 11:36:04 -0300

    atualiza os arquivos

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 function toggleSection(sectionId) {
     const section = document.getElementById(sectionId);
+
     if (section.classList.contains('show')) {
         section.classList.remove('show');
     } else {
+        // Fecha qualquer seção aberta antes de abrir outra (opcional)
+        document.querySelectorAll('.card-section').forEach(sec => sec.classList.remove('show'));
         section.classList.add('show');
     }
 }
